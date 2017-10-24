bed49c9cf4471365880bdf16f6b78a0f615e8273
Moskaevvv <moskaevvv@mail.ru>
2017-10-25 02:00:19 +0300

    Внес правки для защиты на странице формы: поп-апы разместил перед тегом </body>; починил visually-hidden

--- js/form.js
+++ js/form.js
@@ -1,17 +1,13 @@
 "use strict";
 
 var submitButton = document.querySelector(".form__button--submit-button");
-var modalsLayout = document.querySelector(".form__modals");
-var modalDone = document.querySelector(".form__modal--done");
 var modalError = document.querySelector(".form__modal--error");
-var modalDoneButton = modalDone.querySelector("button");
 var modalErorButton = modalError.querySelector("button");
-var form = document.querySelector(form);
 
 submitButton.onclick = function() {
-  modalsLayout.classList.remove("invisible");
+  modalError.classList.remove("visually-hidden");
 };
 
 modalErorButton.onclick = function() {
-  modalsLayout.classList.add("invisible");
+  modalError.classList.add("visually-hidden");
 }
